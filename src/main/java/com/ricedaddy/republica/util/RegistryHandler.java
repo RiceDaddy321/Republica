0b86746bef9ef9e411b3eb73cad7834c3db04df2
RiceDaddy321 <0carlitos2@gmail.com>
2020-12-08 23:35:14 -0600

    Json and Java side of getting armor into the mod has been completed. Now I need to finish the textures.

--- src/main/java/com/ricedaddy/republica/util/RegistryHandler.java
+++ src/main/java/com/ricedaddy/republica/util/RegistryHandler.java
@@ -1,11 +1,14 @@
 package com.ricedaddy.republica.util;
 
 import com.ricedaddy.republica.Republica;
+import com.ricedaddy.republica.armor.ModArmorMaterial;
 import com.ricedaddy.republica.blocks.BlockItemBase;
 import com.ricedaddy.republica.blocks.RubyBlock;
 import com.ricedaddy.republica.items.ItemBase;
 import com.ricedaddy.republica.tools.ModItemTier;
 import net.minecraft.block.Block;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
@@ -29,6 +32,16 @@ public class RegistryHandler {
     //Tools
     public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
             new SwordItem(ModItemTier.RUBY, 2, -2.4f, new Item.Properties().group( Republica.TAB)));
+
+    //Armor
+    public static final RegistryObject<ArmorItem> RUBY_HELMET = ITEMS.register("ruby_helmet", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.HEAD, new Item.Properties().group(Republica.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_CHESTPLATE = ITEMS.register("ruby_chestplate", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.CHEST, new Item.Properties().group(Republica.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_LEGGINGS = ITEMS.register("ruby_leggings", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.LEGS, new Item.Properties().group(Republica.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_BOOTS = ITEMS.register("ruby_boots", () ->
+            new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.FEET, new Item.Properties().group(Republica.TAB)));
 
     //Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
